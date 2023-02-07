7b0673ce372866eeb5e1cd0041593441a46159a8
Ivan Lanuza <ilanuza@iripple.com>
2023-02-08 04:48:07 +0800

    Add Intercom link

--- components/home/ContentProof.js
+++ components/home/ContentProof.js
@@ -2,10 +2,39 @@ import Image from "next/image";
 
 const ContentProof = ({}) => (
   <div className="relative">
-    <div className="block items-center font-sans text-center mt-12 bg-orange-50 py-4 lg:py-12">
-      <div className="grid grid-cols-2 place-content-start text-center pl-4 pr-4 lg:mx-32">
+    <div className=" font-sans  mt-12 bg-orange-50 py-4 lg:py-12">
+      <div className=" grid grid-cols-2  mx-2 lg:mx-0 gap-4ç lg:gap-0">
+        <div className="col-span-1 text-center">
+          <div className="inline-flex place-items-center  ">
+            <div className="">
+              <Image
+                src="/images/home/check-12-years.png"
+                height="120"
+                width="120"
+              />
+            </div>
+            <div className=" inline-flex place-items-center font-base  lg:font-bold pl-1 lg:pl-4  text-xs lg:text-lg  ">
+              12 years of helping small & mid-sized retailers.
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 text-center">
+          <div className="inline-flex place-items-center">
+            <div className="">
+              <Image
+                src="/images/home/50k-support.png"
+                height="120"
+                width="120"
+              />
+            </div>
+            <div className=" inline-flex place-items-center font-base  lg:font-bold pl-1 lg:pl-4  text-xs lg:text-lg  ">
+              50,000+ businesses helped in 170 countries.
+            </div>
+          </div>
+        </div>
+        {/*
         <div className="col-span-1">
-          <div className="grid grid-cols-8">
+          <div className="grid grid-cols-8 border">
             <div className="col-span-2 lg:col-span-1">
               <Image
                 src="/images/home/check-12-years.png"
@@ -19,7 +48,7 @@ const ContentProof = ({}) => (
           </div>
         </div>
         <div className="col-span-1">
-          <div className="grid grid-cols-8">
+          <div className="grid grid-cols-8 border">
             <div className="col-span-2 lg:col-span-1">
               <Image
                 src="/images/home/50k-support.png"
@@ -32,6 +61,7 @@ const ContentProof = ({}) => (
             </div>
           </div>
         </div>
+*/}
       </div>
     </div>
   </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,82 @@
 import "../styles/globals.css";
 import Head from "next/head";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    window.intercomSettings = {
+      app_id: "w09kh1lv",
+      name: "<%= @current_user.name %>", // Full name
+      email: "<%= @current_user.email %>", // Email address
+      user_hash:
+        "<%= OpenSSL::HMAC.hexdigest('sha256', 'vNPLBMEdCqQZ8eNzTzEFanz_S4DTs1Y9GAUWEzEv', @current_user.email) %>",
+      account_name: "<%= @current_account.name%>",
+      subscription_type: "<%= @current_account.subscription_type%>",
+      branch_count: " <%= @current_account.regular_branches.count%>",
+      products_count: "<%= @current_account.products_count %>",
+      invoices_count: "<%= @current_account.invoices_count%>",
+      user_role: "<%= @current_user.role.name%>",
+      cluster_id: "<%=CONFIG['cluster_id']%>",
+      created_at: "<%= @current_account.created_at.to_i%>",
+      trial_start_date: "<%= trial_start_date%>",
+      premium_start_date: "<%=@current_account.premium_start_date%>",
+      trial_expired_at: "<%=@current_account.trial_expired_at%>",
+    };
+
+    // Intercom code snippet
+    (function () {
+      var w = window;
+      var ic = w.Intercom;
+      if (typeof ic === "function") {
+        ic("reattach_activator");
+        ic("update", intercomSettings);
+      } else {
+        var d = document;
+        var i = function () {
+          i.c(arguments);
+        };
+        i.q = [];
+        i.c = function (args) {
+          i.q.push(args);
+        };
+        w.Intercom = i;
+        function l() {
+          var s = d.createElement("script");
+          s.type = "text/javascript";
+          s.async = true;
+          s.src = "https://widget.intercom.io/widget/w09kh1lv";
+          var x = d.getElementsByTagName("script")[0];
+          x.parentNode.insertBefore(s, x);
+        }
+        if (w.attachEvent) {
+          w.attachEvent("onload", l);
+        } else {
+          w.addEventListener("load", l, false);
+        }
+      }
+    })();
+
+    // Call boot method
+    window.Intercom("boot", {
+      app_id: "w09kh1lv",
+    });
+  }, []);
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      window.Intercom("update");
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return (
     <div>
       <Head>
