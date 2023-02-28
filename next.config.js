d6c0025f302ad13f2a703700bb95a5562ac064e6
Ivan Lanuza <ilanuza@iripple.com>
2023-02-28 10:01:06 +0800

    Redirect login

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/login",
+        destination: "https://secure.imonggo.com/login",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
